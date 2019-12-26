135fd2f2ca6f3673c58cca619827a957fb66578a
Sujitha <sujithaece1995@gmail.com>
2019-12-26 16:29:10 +0530

    Update employee.sql

--- employee.sql
+++ employee.sql
@@ -5,6 +5,8 @@ user_name varchar2(50),
 e_mail varchar2(50),
 e_password varchar2(10)
 );
-insert into emp(task_id,user_name,e_mail,e_password)values('1','suji','xxx@gmail.com','yyy');
-insert into emp(task_id,user_name,e_mail,e_password)values('2','siva','zzz@gmail.com','aaa');
+insert into emp(task_id,user_name,e_mail,e_password)values('1','suji','suji@gmail.com','yyy');
+insert into emp(task_id,user_name,e_mail,e_password)values('2','siva','siva@gmail.com','aaa');
+insert into emp(task_id,user_name,e_mail,e_password)values('3','nivi','nivi@gmail.com','bbb');
+insert into emp(task_id,user_name,e_mail,e_password)values('4','kavi','kavi@gmail.com','xxx');
 select * from emp;
